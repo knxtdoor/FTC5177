aec9d54fbe74ede434619367b5293fe192aa2c22
knxtdoor <knxtdoor@aol.com>
2017-01-11 13:36:46 -0500

    Test Color written

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestColor.java
@@ -1,9 +1,28 @@
-package main.java.org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 /**
  * Created by knxtd on 1/10/2017.
  */
 
-public class TestColor {
+public class TestColor extends OpMode{
+ProgbotHardware robot = new ProgbotHardware();
+
+
+    @Override
+    public void init(){
+        telemetry.addData("Color Sensor Test", "Init");
+        telemetry.update();
+    }
+    @Override
+    public void loop(){
+        telemetry.addData("Red",robot.pokerColor.red());
+        telemetry.addData("Blue",robot.pokerColor.blue());
+        telemetry.addData("Green",robot.pokerColor.green());
+        telemetry.addData("Alpha", robot.pokerColor.alpha());
+        telemetry.update();
+
+    }
 
 }
